d039eb20f0d167d9053b6128a5e2bc3b01a587da
taschekuller <idogukanince@gmail.com>
2024-12-26 11:02:53 +0300

    forked and init

--- copilot/README.md
+++
@@ -1,8 +0,0 @@
-# Software Copilot with Chainlit
-
-Run the chainlit application:
-
-```shell
-chainlit run app.py
-```
-This demo uses the custom build feature to serve the frontend through chainlit, although you can serve it through any other server.
--- copilot/app.py
+++
@@ -1,9 +0,0 @@
-import chainlit as cl
-
-
-@cl.on_message
-async def on_message(msg: cl.Message):
-    if cl.context.session.client_type == "copilot":
-        fn = cl.CopilotFunction(name="test", args={"msg": msg.content})
-        res = await fn.acall()
-        await cl.Message(content=res).send()
